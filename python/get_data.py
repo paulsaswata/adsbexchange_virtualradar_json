c8a2229d1e8c517777c3285a388085416d39f204
paulsaswata <pauls4@rpi.edu>
2018-10-30 18:18:32 -0400

    demon upload

---
+++ python/get_data.py
@@ -0,0 +1,26 @@
+
+# importing the requests library 
+import requests 
+import json
+   
+# api-endpoint 
+URL = "https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json"
+
+#callsign for the flight you want to pull data for   
+callsign= "N65620"  
+
+# defining a params dict for the parameters to be sent to the API 
+PARAMS = {'fCall': callsign} 
+  
+# sending get request and saving the response as response object 
+r = requests.get(url = URL, params = PARAMS) 
+  
+# extracting data in json format 
+data = r.json() 
+
+
+#writing to a local jsonfile
+filename= callsign+".txt"
+with open(filename, 'w') as outfile:  
+    json.dump(data, outfile)
+  
